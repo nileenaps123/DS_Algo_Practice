bc42884db9d913b3ca92440ffa6c930d7514cb98
Nileena <nileena.ps123@gmail.com>
2025-11-14 06:58:24 +0530

    implement stack using linkedlist

---
+++ DataStructures/StacksAndQueues/stackLinkedList.js
@@ -0,0 +1,53 @@
+class Node{
+    constructor(val){
+        this.val=val;
+        this.next=null
+    }
+}
+
+class Stack{
+    constructor(){
+        this.first=null;
+        this.last=null;
+        this.size=0;
+    }
+    //insert val at the beginning of list for stack implementation(LIFO)
+    push(val){
+        let newNode=new Node(val);
+        if(!this.size){
+            this.first=newNode;
+            this.last=newNode;
+            
+        }else{
+            let temp=this.first;
+            this.first=newNode;
+            this.first.next=temp;
+        }
+        return ++this.size;
+
+    }
+
+    //Remove a val from beginning of list---stack LIFO
+
+    pop(){
+
+       if(!this.size) return null;
+       let temp=this.first;
+
+       if(this.size===1){
+            
+            this.last=null;
+
+        }
+        this.first=this.first.next;
+        this.length--;
+        return temp.val;
+    }
+}
+
+let stack=new Stack();
+stack.push(200);
+stack.push(201);
+stack.push(202);
+console.log(stack);
+
