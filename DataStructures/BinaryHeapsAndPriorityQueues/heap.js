287cc8e0bc9a90238a7df27930ed454bfdb4a258
Nileena <nileena.ps123@gmail.com>
2025-12-03 04:29:37 +0530

    Max Binary Heap

--- DataStructures/BinaryHeapsAndPriorityQueues/heap.js
+++ DataStructures/BinaryHeapsAndPriorityQueues/heap.js
@@ -1,6 +1,6 @@
 class MaxBinaryHeap{
     constructor(){
-        this.values=[41,39,33,18,27,12];
+        this.values=[];
     }
 
     insert(val){
@@ -22,11 +22,79 @@ class MaxBinaryHeap{
         }
         return this.values;
     }
+    extractMax(){
+        let max=this.values[0];
+        let end=this.values.pop();
+        if(this.values.length>0){
+            this.values[0]=end;
+            this.sinkDown();
+
+        }
+
+        return max;      
+    }
+    sinkDown(){
+        let idx=0;
+        let element=this.values[0];
+        while(true){
+            let swap=null;
+            let leftIdx=2*idx+1;
+            let rightIdx=2*idx+2; 
+            let length=this.values.length; 
+            let left,right;
+            
+            if( leftIdx<length){
+                left=this.values[leftIdx];
+                if(left>element){
+                    swap=leftIdx;
+                }
+            }
+            if( rightIdx<length){
+                right=this.values[rightIdx];
+                if((swap===null && right>element) ||
+                    (swap!==null && right>left)
+                    ){
+                    swap=rightIdx;
+                }
+            }   
+
+            if(swap===null)break;   
+            
+            this.values[idx]=this.values[swap];
+            this.values[swap]=element;
+            idx=swap;
+
+    }
+
+}
 }
 
 let heap=new MaxBinaryHeap();
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(55);
 
-console.log(heap.insert(55));
-console.log(heap.insert(1));
-console.log(heap.insert(45));
-console.log(heap.insert(199));
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+//console.log(heap.insert(45));
+//console.log(heap.insert(199));
