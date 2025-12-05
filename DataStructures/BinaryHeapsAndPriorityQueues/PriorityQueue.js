3cc397236c05db21114fc095bd5d77353ab3dfff
Nileena <nileena.ps123@gmail.com>
2025-12-05 05:55:54 +0530

    priorityQueue

---
+++ DataStructures/BinaryHeapsAndPriorityQueues/PriorityQueue.js
@@ -0,0 +1,88 @@
+class PriorityQueue{ //min binary heap
+    constructor(){
+        this.values=[];
+    }
+
+        enqueue(val,priority){
+        let node=new Node(val,priority);
+        this.values.push(node);
+        return this.bubbleUp();
+    }
+    
+    bubbleUp(){
+        let idx=this.values.length-1;
+        const  element=this.values[idx];
+        while(idx>0){
+            let parentIdx=Math.floor((idx-1)/2);
+            let parent=this.values[parentIdx];
+            if(parent.priority<=element.priority) break;
+            this.values[parentIdx]=element;
+            this.values[idx]=parent;
+            idx=parentIdx;
+        }
+        return this.values;
+    }
+
+    dequeue(){
+        const min=this.values[0];
+        const end=this.values.pop();
+        if(this.values.length>0){
+            this.values[0]=end;
+            this.sinkDown();
+        }
+        return min;
+    }
+
+    sinkDown(){
+        let element=this.values[0];
+        let idx=0;
+        const length=this.values.length;
+
+        while(true){
+
+            let swap=null;
+            let leftIdx=2*idx+1;
+            let rightIdx=2*idx+2;
+            let left,right;
+                   
+
+            if(leftIdx< length){
+               left=this.values[leftIdx];
+                if(left.priority<element.priority){
+                    swap=leftIdx;
+                }
+            }
+
+            if(rightIdx< length){
+                right=this.values[rightIdx];
+                if((swap===null && right.priority<element.priority)||
+                   (swap!==null && right.priority<left.priority)    
+                 ){
+                    swap=rightIdx;
+                }
+            }
+
+            if (swap===null) break;
+            
+            this.values[idx]=this.values[swap];
+            this.values[swap]=element;
+            idx=swap;
+        }
+    }
+}
+
+class Node{
+    constructor(val,priority){
+        this.val=val;
+        this.priority=priority;
+    }
+}
+
+let ER = new PriorityQueue();
+ER.enqueue("common cold",5)
+ER.enqueue("gunshot wound", 1)
+ER.enqueue("high fever",4)
+ER.enqueue("broken arm",2)
+ER.enqueue("glass in foot",3)
+
+console.log(ER.dequeue());
