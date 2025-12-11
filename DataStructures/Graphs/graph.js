14763f45f3862c106c9bf4ddb7b8bc7b3fa3e315
Nileena <nileena.ps123@gmail.com>
2025-12-11 07:46:38 +0530

    graph-depthFirstRecursiveTraversal

--- DataStructures/Graphs/graph.js
+++ DataStructures/Graphs/graph.js
@@ -26,9 +26,32 @@ class Graph{
         }
         delete this.adjacencyList[v];
     }
+
+    //dfs graph traversal-recursive
+
+    depthFirstRecursive(start){
+
+        let result=[];
+        let visitFlag={};
+        const adjacencyList = this.adjacencyList;
+
+        function DFS(vertex){
+            if(!vertex) return null ;
+
+            result.push(vertex);
+            visitFlag[vertex]=true;
+
+            adjacencyList[vertex].forEach(neighbour=>{
+                if(!visitFlag[neighbour]) return DFS(neighbour);
+            });
+        }
+        DFS(start);
+
+        return result;
+    }
 }
 
-let g = new Graph();
+/* let g = new Graph();
 g.addVertex("India");
 g.addVertex("Tokyo");
 g.addVertex("Japan");
@@ -37,5 +60,34 @@ g.addEdge("Tokyo", "India");
 g.addEdge("Tokyo", "China");
 g.addEdge("India", "Japan");
 g.removeVertex("India")
+console.log(g) */
+
+let g = new Graph();
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B","D")
+g.addEdge("C","E")
+g.addEdge("D","E")
+g.addEdge("D","F")
+g.addEdge("E","F")
 console.log(g)
+console.log(g.depthFirstRecursive("A"));
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
+
 
