b0d7bae4491fbd2516cd008d991ef3736adf808e
Nileena <nileena.ps123@gmail.com>
2026-01-23 12:47:16 +0530

    container with most water

---
+++ Neet150/Two_Pointers/maxArea.js
@@ -0,0 +1,28 @@
+//TC O(n)  ,SC -O(1)
+
+function maxArea(heights){
+
+    let res;
+    let l=0,r=heights.length-1;
+    let area,maxArea=0,width;
+
+    while (l<r){
+
+        area=Math.min(heights[l],heights[r])*(r-l);
+        maxArea=Math.max(maxArea,area);
+
+        if(heights[l]<heights[r]){
+            l++;
+        }else{
+            r--;
+        }
+
+
+    }
+    return maxArea;
+
+}
+
+console.log(maxArea([1,7,2,5,4,7,3,6]));
+console.log(maxArea([2,2,2]));
+//console.log(maxArea());
